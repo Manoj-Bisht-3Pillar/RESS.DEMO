3922c26f03d05ca2cee4e9b0c16246c14906943c
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-04-07 13:34:23 +0530

    updated accordint to the rrequirements

--- RESS.DEMO.Web/Scripts/app.js
+++ RESS.DEMO.Web/Scripts/app.js
@@ -25,6 +25,6 @@ phonecatApp.config(['$routeProvider',
         controller: 'PhoneDetailCtrl'
       }).
       otherwise({
-          redirectTo: '/Homes/:client/:language'
+          redirectTo: '/Homes/2/en'
       });
   }]);
