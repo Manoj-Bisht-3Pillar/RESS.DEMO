94117f974a32791679bde58abf362c24b0852ab6
ankit rustagi <ankit.rustagi@3pillarglobal.com>
2014-05-08 16:12:57 +0530

    Pushing Alert Service

--- RESS.DEMO.Web/Scripts/AlertHandlingService/alertDirective.js
+++ RESS.DEMO.Web/Scripts/AlertHandlingService/alertDirective.js
@@ -8,7 +8,7 @@ alertDirective.directive('alertDirective', function () {
         transclude: true,
         compile: function(element, attrs){
             var tag = '<div ng-repeat="(key,val) in alerts" class="alert {{key}}">' +
-                        '<h1>hello world</h1>' +
+                        '<div ng-repeat="msg in val">{{msg}}</div>' +
                       '</div>';
             element.append(tag);
         },
--- RESS.DEMO.Web/Scripts/DepositCheck/depositCheckController.js
+++ RESS.DEMO.Web/Scripts/DepositCheck/depositCheckController.js
@@ -4,11 +4,16 @@
 
 var checkDepositControllers = angular.module('checkDepositControllers', []);
 checkDepositControllers.controller('checkDepositControllers', ['$scope', 'alertService', function ($scope, alertService) {
-    $scope.default_cheque_number = "111111";
+    $scope.default_cheque_number = "123456";
+    $scope.alerts = alertService.alerts;
     $scope.validate_cheque = function (customer) {
         if ($scope.default_cheque_number != customer.ChequeNumber) {
             alertService.errorEvent("Check number incorrect");
+        }
+        else {
+            alertService.successEvent("Check Number is Correct");
         };
+
 
     };
 }]);
